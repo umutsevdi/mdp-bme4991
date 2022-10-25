2312a632a97e467ca285ecc9e65ec9120a32a9ba
Umut Sevdi <sevdiumut@gmail.com>
2022-10-25 23:09:30 +0300

    Added test client

--- embeded_system/server/header/server.h
+++ embeded_system/server/header/server.h
@@ -9,8 +9,18 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#endif /* ifndef SYMBOL */
 
+enum direction {
+  DIRECTION_NONE,
+  DIRECTION_LEFT,
+  DIRECTION_DOWN,
+  DIRECTION_UP,
+  DIRECTION_RIGHT,
+  DIRECTION_UP_LEFT,
+  DIRECTION_DOWN_LEFT,
+  DIRECTION_UP_RIGHT,
+  DIRECTION_DOWN_RIGHT,
+};
 /**
  * configuration related information about the UDP
  * listener server
@@ -20,9 +30,20 @@ typedef struct SV_SERVER_CONFIG {
   int max_line;
 } sv_conf;
 
+typedef struct SV_MOTION_DATA {
+  short dir_x;
+  short dir_y;
+  unsigned long timestamp;
+} sv_motion;
+
 /**
  * creates a UDP server, must be executed in a separate
  * thread
  * @param arg {@link server_config}
  */
 void sv_listen(void *arg);
+
+enum direction sv_motion_get_dir(short dir_x, short dir_y);
+
+#endif /* ifndef UDP_SERVER */
+
--- embeded_system/server/src/server.c
+++ embeded_system/server/src/server.c
@@ -1,10 +1,9 @@
 #include "server.h"
+#include <unistd.h>
 
-// Driver code
 void sv_listen(void *args) {
   sv_conf *server_config = (sv_conf *)args;
   int sockfd;
-  char buffer[server_config->max_line];
   char *hello = "Hello from server";
   struct sockaddr_in servaddr, cliaddr;
 
@@ -32,13 +31,27 @@ void sv_listen(void *args) {
 
   len = sizeof(cliaddr); // len is value/result
   while (1) {
-
+    // @TODO add threading here maybe
+    char buffer[server_config->max_line];
     n = recvfrom(sockfd, (char *)buffer, server_config->max_line, MSG_WAITALL,
                  (struct sockaddr *)&cliaddr, &len);
-    buffer[n] = '\0';
-    printf("Client : %s\n", buffer);
-    sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM,
-           (const struct sockaddr *)&cliaddr, len);
-    printf("Hello message sent.\n");
+    // @TODO Handle this text processing in a separate thread
+    if (n > 0) {
+      // Set buffer as `null terminated string`
+      buffer[n] = '\0';
+
+      printf("Client %d:%d\t: %s\n", cliaddr.sin_addr.s_addr, cliaddr.sin_port,
+             buffer);
+      sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM,
+             (const struct sockaddr *)&cliaddr, len);
+      printf("Hello message sent.\n");
+    }
   }
 }
+
+/**
+ * parses incoming UDP data into a valid struct
+ * @param buffer incoming message as char array
+ * @return struct data
+ */
+sv_motion *parse(char *buffer) { return NULL; }
